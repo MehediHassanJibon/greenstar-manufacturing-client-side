cd0acecca9a2ef149613e9ffc51a43cda11370ad
Mehedi Hassan Jibon <mhj124177@gmail.com>
2022-05-28 21:29:35 +0600

    hompage added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-import { ToastContainer} from 'react-toastify';
+import Navbar from './Pages/Shared/Navbar';
+import { Routes, Route } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Purchase from './Pages/Purchase/Purchase';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Login/Register';
+import RequireAuth from './Pages/RequireAuth/RequireAuth';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import MyOrders from './Pages/Dashboard/MyOrders';
+import AddAReview from './Pages/Dashboard/AddAReview';
+import MyProfile from './Pages/Dashboard/MyProfile';
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+import Payment from './Pages/Dashboard/Payment';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">
-    Wellcome To Green Manufacturing Limited
-  </h1>
-  <button className="btn btn-primary">Button</button>
-  <ToastContainer></ToastContainer>
+    <div className='max-w-7xl mx-auto px-10'>
+      <QueryClientProvider client={queryClient}>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/home' element={<Home></Home>}></Route>
+          <Route path='/purchase/:productId' element={
+            <RequireAuth>
+              <Purchase></Purchase>
+            </RequireAuth>
+          }></Route>
+          <Route path='/dashboard' element={
+            <RequireAuth>
+              <Dashboard></Dashboard>
+            </RequireAuth>}>
+            <Route index element={<MyOrders></MyOrders>} />
+            <Route path='myOrders' element={<MyOrders></MyOrders>} />
+            <Route path='payment/:id' element={<Payment></Payment>} />
+            <Route path='addAReview' element={<AddAReview></AddAReview>} />
+            <Route path='myProfile' element={<MyProfile></MyProfile>} />
+          </Route>
+          <Route path='/blogs' element={<Home></Home>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
+          <Route path='/register' element={<Register></Register>}></Route>
+        </Routes>
+        <ToastContainer></ToastContainer>
+      </QueryClientProvider>
     </div>
   );
 }
