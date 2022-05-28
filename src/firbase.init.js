880358eb93786ceb99ede14ae9148aea7c055706
Mehedi Hassan Jibon <mhj124177@gmail.com>
2022-05-28 17:43:41 +0600

    configuring envirorment file

--- src/firbase.init.js
+++ src/firbase.init.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBDwccmS1A9lDdK9aP6vaYvSQ2c6zBL090",
-  authDomain: "greenstar-manufacturing.firebaseapp.com",
-  projectId: "greenstar-manufacturing",
-  storageBucket: "greenstar-manufacturing.appspot.com",
-  messagingSenderId: "478518614416",
-  appId: "1:478518614416:web:7087fc86f1f766593a7b15"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
