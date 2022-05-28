7d45b278bea3c2fd12cf440d85930aab5a2e763f
Mehedi Hassan Jibon <mhj124177@gmail.com>
2022-05-28 22:28:47 +0600

    profile created

--- src/Pages/Dashboard/MyOrder.js
+++ src/Pages/Dashboard/MyOrder.js
@@ -45,7 +45,9 @@ const MyOrder = ({ order, index }) => {
             <td>{order.orderQuantity}</td>
             <td>${order.totalCost}</td>
             <td>
-               
+            {
+                    order.paid ? <p className='text-green-500'>Paid</p> : <button className='btn btn-xs bg-red-500' onClick={() => handleCancelOrder(order._id)}>Cancel</button>
+                }
             </td>
             <td>
                 {
